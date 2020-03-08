45a56c824113dcdd0e8793398be50c154ceed93d
Griffin Poole <griffinpoole5@gmail.com>
2020-03-08 19:49:14 -0400

    added random room generation and tunnel joining alg to make_map function

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from map_objects.rectangle import Rect
 from map_objects.tile import Tile
 
@@ -12,14 +14,54 @@ class GameMap:
 
         return tiles
 
-    def make_map(self):
-        # Create two rooms for test
-        room1 = Rect(20, 15, 10, 15)
-        room2 = Rect(35, 15, 10, 15)
+    def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player):
+       
+       rooms = []
+       num_rooms = 0
 
-        self.create_room(room1)
-        self.create_room(room2)
-        self.create_h_tunnel(25, 40, 23)
+       for r in range(max_rooms):
+           # random width and height
+           w = randint(room_min_size, room_max_size)
+           h = randint(room_min_size, room_max_size)
+           #random position without going OOB
+           x = randint(0, map_width - w - 1)
+           y = randint(0, map_height - h - 1)
+
+           # Rect class provides support for working with our rooms
+           new_room = Rect(x, y, w, h)
+
+           #Check to see if room intersects with another
+           for other_room in rooms:
+                if new_room.intersect(other_room):
+                   break
+                else:
+                    #no intersections, valid room
+                    # draw the room
+                    self.create_room(new_room)
+                    (new_x, new_y) = new_room.center()
+
+                    if num_rooms == 0:
+                        # puts player in first room
+                        player.x = new_x
+                        player.y = new_y
+                    else:
+                        #for all rooms after first, need to connect via tunnels
+                        #center coords of prev room
+                        (prev_x, prev_y) = rooms[num_rooms - 1].center()
+
+                        # flip a coin
+                        if randint(0, 1) == 1:
+                            # first move horizontally, then vertical
+                            self.create_h_tunnel(prev_x, new_x, prev_y)
+                            self.create_v_tunner(prev_y, new_y, new_x)
+                        else:
+                            # first move vertical, than horizontal
+                            self.create_v_tunner(prev_y, new_y, prev_x)
+                            self.create_h_tunnel(prev_x, new_x, new_y)
+                    
+                    #append new room to room list
+                    rooms.append(new_room)
+                    num_rooms += 1
 
     def create_room(self, room):
         # makes all tiles within room dimensions passable
