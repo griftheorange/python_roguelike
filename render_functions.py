7161b58063fbaf27dc0e2ddc97a0e8115611fdae
Griffin Poole <griffinpoole5@gmail.com>
2020-03-08 19:06:49 -0400

    added map tiles, drew tiles, walls block player traveling through

--- render_functions.py
+++ render_functions.py
@@ -1,6 +1,17 @@
 import tcod as libtcod
 
-def render_all(con, entities, screen_width, screen_height):
+def render_all(con, entities, game_map, screen_width, screen_height, colors):
+    # Draw all tiles in map
+    for y in range(game_map.height):
+        for x in range(game_map.width):
+            # Tracks whether tile blocks sight
+            wall = game_map.tiles[x][y].block_sight
+    
+            if wall:
+                libtcod.console_set_char_background(con, x, y, colors.get('dark_wall'), libtcod.BKGND_SET)
+            else:
+                libtcod.console_set_char_background(con, x, y, colors.get('dark_ground'), libtcod.BKGND_SET)
+
     # Iterate through entities, draw all
     for entity in entities:
         draw_entity(con, entity)
