1abe2bd0cfd5d8ee9ba12b34e1bd01bdc0f517eb
Griffin Poole <griffinpoole5@gmail.com>
2020-03-08 17:10:06 -0400

    Window loops through render until ESC key event detected, sys_check_for_event implemented capturing mouse and key events, need system to respond

--- engine.py
+++ engine.py
@@ -1,9 +1,30 @@
 import tcod as libtcod
 
-
 def main():
-    print('Hello World!')
+    screen_width = 80
+    screen_height = 50
 
+    player_x = int(screen_width/2)
+    player_y = int(screen_height/2)
+
+    libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+
+    libtcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
+
+    key = libtcod.Key()
+    mouse = libtcod.Mouse()
+
+    while not libtcod.console_is_window_closed():
+        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
+
+        libtcod.console_set_default_foreground(0, libtcod.white)
+        libtcod.console_put_char(0, player_x, player_y, '@', libtcod.BKGND_NONE)
+        libtcod.console_flush()
+
+        key = libtcod.console_check_for_keypress()
+
+        if key.vk == libtcod.KEY_ESCAPE:
+            return True
 
 if __name__ == '__main__':
     main()
