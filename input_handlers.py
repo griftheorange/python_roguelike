1ddac3bbfb12fa708dbfa5b9b32d0d271f0fb9e7
Griffin Poole <griffinpoole5@gmail.com>
2020-03-08 17:26:44 -0400

    added keypress handler file and function

---
+++ input_handlers.py
@@ -0,0 +1,25 @@
+import tcod as libtcod
+
+# Number of spaces a player increments per move
+SPEED = 1
+def handle_keys(key):
+    # Player Movement
+    if key.vk == libtcod.KEY_UP:
+        return {'move': (0, -SPEED)}
+    elif key.vk == libtcod.KEY_DOWN:
+        return {'move': (0, SPEED)}
+    elif key.vk == libtcod.KEY_LEFT:
+        retunr {'move': (-SPEED, 0)}
+    elif key.vk == libtcod.KEY_RIGHT:
+        return {'move': (SPEED, 0)}
+
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        # Alt+Enter toggles fullscreen
+        return {'fullscreen': True}
+
+    elif key.vk = libtcod.KEY_ESCAPE:
+        # Exit the game
+        return {'exit': True}
+    
+    # No press
+    return {}
