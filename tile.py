d20bf8dc55953351b0def99024d9ab958b0ac90a
Griffin Poole <griffinpoole5@gmail.com>
2020-03-08 18:47:00 -0400

    added tile class, stores blocked and transparency attributes of tiles

---
+++ tile.py
@@ -0,0 +1,13 @@
+class Tile:
+    """
+    A tile on a map. It may or may be blocked/transparent
+    """
+
+    def __init__(self, blocked, block_sight=None):
+        self.blocked = blocked
+
+        # By default, blocked tiles also block sight
+        if block_sight is None:
+            block_sight = blocked
+        
+        self.block_sight = block_sight
