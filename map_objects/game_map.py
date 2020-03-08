580c4a0c2c22af7a0ba28a7f72518f951ceaf0f5
Griffin Poole <griffinpoole5@gmail.com>
2020-03-08 19:11:06 -0400

    removed manual wall from testing stage

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -7,14 +7,7 @@ class GameMap:
         self.tiles = self.initialize_tiles()
     
     def initialize_tiles(self):
-        tiles = [[Tile(False) for y in range(self.height)] for x in range(self.width)]
-
-        tiles[30][22].blocked = True
-        tiles[30][22].block_sight = True
-        tiles[31][22].blocked = True
-        tiles[31][22].block_sight = True
-        tiles[32][22].blocked = True
-        tiles[32][22].block_sight = True
+        tiles = [[Tile(True) for y in range(self.height)] for x in range(self.width)]
 
         return tiles
 
