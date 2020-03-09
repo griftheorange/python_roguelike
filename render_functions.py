ea040e1cc2a0869c12279eecf3444fea576ed5a2
Griffin Poole <griffinpoole5@gmail.com>
2020-03-09 12:22:36 -0400

    added fov to render yellow for player

--- render_functions.py
+++ render_functions.py
@@ -1,16 +1,24 @@
 import tcod as libtcod
 
-def render_all(con, entities, game_map, screen_width, screen_height, colors):
+def render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, screen_height, colors):
     # Draw all tiles in map
-    for y in range(game_map.height):
-        for x in range(game_map.width):
-            # Tracks whether tile blocks sight
-            wall = game_map.tiles[x][y].block_sight
-    
-            if wall:
-                libtcod.console_set_char_background(con, x, y, colors.get('dark_wall'), libtcod.BKGND_SET)
-            else:
-                libtcod.console_set_char_background(con, x, y, colors.get('dark_ground'), libtcod.BKGND_SET)
+    if fov_recompute:
+        for y in range(game_map.height):
+            for x in range(game_map.width):
+                # Tracks whether tile blocks sight
+                visible = libtcod.map_is_in_fov(fov_map, x, y)
+                wall = game_map.tiles[x][y].block_sight
+        
+                if visible:
+                    if wall:
+                        libtcod.console_set_char_background(con, x, y, colors.get('light_wall'), libtcod.BKGND_SET)
+                    else:
+                        libtcod.console_set_char_background(con, x, y, colors.get('light_ground'), libtcod.BKGND_SET)
+                else:
+                    if wall:
+                        libtcod.console_set_char_background(con, x, y, colors.get('dark_wall'), libtcod.BKGND_SET)
+                    else:
+                        libtcod.console_set_char_background(con, x, y, colors.get('dark_ground'), libtcod.BKGND_SET)
 
     # Iterate through entities, draw all
     for entity in entities:
