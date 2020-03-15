35a07d664f0ba9c8199ad6aea1b408333d53f963
Griffin Poole <griffinpoole5@gmail.com>
2020-03-14 20:36:19 -0400

    Added random generation of Trolls and orcs in rooms

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -1,5 +1,7 @@
+import tcod as libtcod
 from random import randint
 
+from entity import Entity
 from map_objects.rectangle import Rect
 from map_objects.tile import Tile
 
@@ -14,7 +16,7 @@ class GameMap:
 
         return tiles
 
-    def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player):
+    def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities, max_monsters_per_room):
        
        rooms = []
        num_rooms = 0
@@ -59,6 +61,7 @@ class GameMap:
                        self.create_v_tunnel(prev_y, new_y, prev_x)
                        self.create_h_tunnel(prev_x, new_x, new_y)
                     
+               self.place_entities(new_room, entities, max_monsters_per_room)
                #append new room to room list
                rooms.append(new_room)
                num_rooms += 1
@@ -77,6 +80,23 @@ class GameMap:
     def create_v_tunnel(self, y1, y2, x):
         for y in range(min(y1, y2), max(y1, y2) + 1):
             self.tiles[x][y].dig()
+
+    def place_entities(self, room, entities, max_monsters_per_room):
+        # Get random number for this room's monsters
+        number_mons = randint(0, max_monsters_per_room)
+
+        for i in range(number_mons):
+            # Choose random location in the room
+            x = randint(room.x1 + 1, room.x2 - 1)
+            y = randint(room.y1 + 1, room.y2 - 1)
+
+            if not any([entity for entity in entities if entity.x == x and entity.y == y]):
+                if randint(0, 100) < 80:
+                    monster = Entity(x, y, 'o', libtcod.desaturated_green)
+                else:
+                    monster = Entity(x, y, 'T', libtcod.darker_green)
+                
+                entities.append(monster)
 
     def is_blocked(self, x, y):
         if self.tiles[x][y].blocked:
