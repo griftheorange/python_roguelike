e0a6667cdf5178967df34b22fc8fa2dd219290f5
Griffin Poole <griffinpoole5@gmail.com>
2020-03-14 20:47:49 -0400

    monsters block player movement, movement into monsters kicks them in the shins

--- entity.py
+++ entity.py
@@ -2,11 +2,13 @@ class Entity:
     """
     A generic object for game entities, players, enemies, items
     """
-    def __init__(self, x, y, char, color):
+    def __init__(self, x, y, char, color, name, blocks=False):
         self.x = x
         self.y = y
         self.char = char
         self.color = color
+        self.name = name
+        self.blocks = blocks
 
     def move(self, dx, dy):
         self.x += dx
