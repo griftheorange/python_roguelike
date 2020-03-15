79d4657baf0899be1427d204d3265f8607356d13
Griffin Poole <griffinpoole5@gmail.com>
2020-03-14 22:13:16 -0400

    player can move and attack in diagonals now

--- input_handlers.py
+++ input_handlers.py
@@ -4,14 +4,24 @@ import tcod as libtcod
 SPEED = 1
 def handle_keys(key):
     # Player Movement
-    if key.vk == libtcod.KEY_UP:
-        return {'move': (0, -SPEED)}
-    elif key.vk == libtcod.KEY_DOWN:
-        return {'move': (0, SPEED)}
-    elif key.vk == libtcod.KEY_LEFT:
-        return {'move': (-SPEED, 0)}
-    elif key.vk == libtcod.KEY_RIGHT:
-        return {'move': (SPEED, 0)}
+    key_char = chr(key.c)
+
+    if key.vk == libtcod.KEY_UP or key_char == 'k':
+        return {'move': (0, -1)}
+    elif key.vk == libtcod.KEY_DOWN or key_char == 'j':
+        return {'move': (0, 1)}
+    elif key.vk == libtcod.KEY_LEFT or key_char == 'h':
+        return {'move': (-1, 0)}
+    elif key.vk == libtcod.KEY_RIGHT or key_char == 'l':
+        return {'move': (1, 0)}
+    elif key_char == 'y':
+        return {'move': (-1, -1)}
+    elif key_char == 'u':
+        return {'move': (1, -1)}
+    elif key_char == 'b':
+        return {'move': (-1, 1)}
+    elif key_char == 'n':
+        return {'move': (1, 1)}
 
     # Non-Movement
     if key.vk == libtcod.KEY_ENTER and key.lalt:
