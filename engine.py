41a91b78ec41eff6e07d03ea207a38a2b9cb86e9
Griffin Poole <griffinpoole5@gmail.com>
2020-03-23 17:42:41 -0400

    added game initialize function and corrected engine

--- engine.py
+++ engine.py
@@ -1,24 +1,16 @@
 import tcod as libtcod
 
-from components.fighter import Fighter
-from components.inventory import Inventory
 from death_functions import kill_monster, kill_player
-from entity import Entity, get_blocking_entities_at_location
+from entity import get_blocking_entities_at_location
 from fov_functions import initialize_fov, recompute_fov
-from game_messages import Message, MessageLog
+from game_messages import Message
 from game_states import GameStates
 from input_handlers import handle_keys, handle_mouse
-from loader_functions.initialize_new_game import get_constants
-from map_objects.game_map import GameMap
-from  render_functions import clear_all, render_all, RenderOrder
+from loader_functions.initialize_new_game import get_constants, get_game_variables
+from render_functions import clear_all, render_all
 
 def main():
-    constants = get_constants
-
-    fighter_component = Fighter(hp=30, defense=2, power=5)
-    inventory_component = Inventory(26)
-    player = Entity(0, 0, '@', libtcod.white, 'Player', blocks=True, render_order=RenderOrder.ACTOR, fighter=fighter_component, inventory=inventory_component)
-    entities = [player]
+    constants = get_constants()
 
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
@@ -27,19 +19,15 @@ def main():
     con = libtcod.console_new(constants['screen_width'], constants['screen_height'])
     panel = libtcod.console_new(constants['screen_width'], constants['panel_height'])
 
-    game_map = GameMap(constants['map_width'], constants['map_height'])
-    game_map.make_map(constants['max_rooms'], constants['room_min_size'], constants['room_max_size'], constants['map_width'], constants['map_height'], player, entities, constants['max_monsters_per_room'], constants['max_items_per_room'])
+    player, entities, game_map, message_log, game_state = get_game_variables(constants)
 
     fov_recompute = True
 
     fov_map = initialize_fov(game_map)
 
-    message_log = MessageLog(constants['message_x'], constants['message_width'], constants['message_height'])
-
     key = libtcod.Key()
     mouse = libtcod.Mouse()
 
-    game_state = GameStates.PLAYERS_TURN
     previous_game_state = game_state
 
     while not libtcod.console_is_window_closed():
@@ -48,7 +36,7 @@ def main():
         if fov_recompute:
             recompute_fov(fov_map, player.x, player.y, constants['fov_radius'], constants['fov_light_walls'], constants['fov_algorithm'])
 
-        render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, message_log, screen_width, constants['screen_height'], constants['bar_width'], constants['panel_height'], constants['panel_y'], mouse, constants['colors'], game_state)
+        render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, message_log, constants['screen_width'], constants['screen_height'], constants['bar_width'], constants['panel_height'], constants['panel_y'], mouse, constants['colors'], game_state)
         fov_recompute = False
 
         libtcod.console_flush()
--- loader_functions/initialize_new_game.py
+++ loader_functions/initialize_new_game.py
@@ -1,5 +1,16 @@
 import tcod as libtcod
 
+from components.fighter import Fighter
+from components.inventory import Inventory
+
+from entity import Entity
+
+from game_messages import GamesStates
+
+from map_objects.game_map import GameMap
+
+from render_functions import RenderOrder
+
 def get_constants():
     window_title = 'Roguelike Tutorial Revised'
 
@@ -36,7 +47,7 @@ def get_constants():
         'light_ground': libtcod.Color(200, 180, 50)
     }
 
-    constant = {
+    constants = {
         'window_title': window_title,
         'screen_width': screen_width,
         'screen_height': screen_height,
@@ -60,3 +71,18 @@ def get_constants():
     }
 
     return constants
+
+def get_game_variables(constants):
+    fighter_component = Fighter(hp=30, defense=2, power=5)
+    inventory_component = Inventory(26)
+    player = Entity(0, 0, '@', libtcod.white, 'Player', blocks=True, render_order=RenderOrder.ACTOR, fighter=fighter_component, inventory=inventory_component)
+    entities = [player]
+
+    game_map = GameMap(constants['map_width'], constants['map_height'])
+    game_map.make_map(constants['max_rooms'], constants['room_min_size'], constants['room_max_size'], constants['map_width'], contants['map_height'], player, entities, constants['max_monsters_per_room'], constants['max_items_per_room'])
+
+    message_log = MessageLog(constants['message_x'], constants['message_width'], constants['message_height'])
+
+    game_state = GamesStates.PLAYERS_TURN
+
+    return player, entities, game_map, message_log, game_state
